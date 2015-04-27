05e466cf39339028be4b390078b3bf76e81b3967
WTFox <Anthonyfox1988@Gmail.com>
2015-04-26 21:40:03 -0500

    Initial commit

---
+++ pushover.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+try:
+    #python 2
+    from urllib2 import urlopen
+    from urlparse import urljoin
+    from urllib import urlencode
+except ImportError:
+    #python 3
+    from urllib.request import urlopen
+    from urllib.parse import urljoin, urlencode
+import json
+import os
+
+PUSHOVER_API = "https://api.pushover.net/1/"
+
+class PushoverError(Exception): pass
+
+def pushover(**kwargs):
+    assert 'message' in kwargs
+
+    if not 'token' in kwargs:
+        kwargs['token'] = os.environ['PUSHOVER_TOKEN']
+    if not 'user' in kwargs:
+        kwargs['user'] = os.environ['PUSHOVER_USER']
+
+    url = urljoin(PUSHOVER_API, "messages.json")
+    data = urlencode(kwargs)
+    binary_data = data.encode('ascii')
+    req = urlopen(url, binary_data)
+    data = json.loads(req.read().decode('utf-8'))
+    if data['status'] != 1:
+        raise PushoverError(output)
+
+
+if __name__ == "__main__":
+    pass
